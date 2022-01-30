d735cad112f361527b798465a8a43b37a0659493
Sergey Risovaniy <risovaniysa@gmail.com>
2022-01-30 17:58:07 +0300

    Finished the template for ua

--- bin/create_UI_UA.py
+++ bin/create_UI_UA.py
@@ -66,6 +66,41 @@ def create_table_fmt(document, count_rows, count_cols):
 #                   UI LOGIC                        #
 #                                                   #
 #####################################################
+# import os
+#
+# import docx
+# from docx.shared import Inches
+#
+#
+# def find_table_in_ui_template(doc):
+#     for table in doc.tables:
+#         if table.cell(0, 0).text == 'п/п':
+#             return table
+#
+#
+# def open_docx_file(path):
+#     return docx.Document(path)
+#
+#     # return doc
+#
+#
+# if __name__ == '__main__':
+#     doc = open_docx_file(r'C:\Users\Risov\Desktop\Prog\auto_ui_ua\auto_UI_UA\resources\UI_template.docx')
+#
+#     ui_table = find_table_in_ui_template(doc)
+#
+#     ui_table.add_row()
+#     ui_table.add_row()
+#     ui_table.add_row()
+#     ui_table.add_row()
+#     ui_table.add_row()
+#
+#     ui_table.style = 'table_ui' #ui_table._TableStyle('table_ui')
+#     ui_table.style = 'table_ui_title' #ui_table._TableStyle('table_ui')
+#
+#     doc.save(f"{os.sep}test_ui_table.docx")
+
+
 # def create_column_names_ui(current_table):
 #     """Create names for column in table of UI, prepare a table to filling.
 #
