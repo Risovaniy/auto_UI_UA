4368fe2aab3fbc816229f9904eb5e661f3ba7fc2
SergeyRisovaniy <risovaniysa@gmail.com>
2022-02-10 16:44:28 +0300

    Load template docx in utf-8

--- trash/checking_testing_hipo.py
+++ trash/checking_testing_hipo.py
@@ -1,45 +1,7 @@
-# import os
-#
-# import docx
-# from docx.shared import Inches
-#
-#
-# def find_table_in_ui_template(doc):
-#     for table in doc.tables:
-#         if table.cell(0, 0).text == 'п/п':
-#             return table
-#
-#
-# def open_docx_file(path):
-#     return docx.Document(path)
-#
-#     # return doc
-#
-#
-# if __name__ == '__main__':
-#     doc = open_docx_file(r'C:\Users\Risov\Desktop\Prog\auto_ui_ua\auto_UI_UA\resources\UI_template.docx')
-#
-#     ui_table = find_table_in_ui_template(doc)
-#
-#     ui_table.add_row()
-#     ui_table.add_row()
-#     ui_table.add_row()
-#     ui_table.add_row()
-#     ui_table.add_row()
-#
-#     ui_table.style = 'table_ui' #ui_table._TableStyle('table_ui')
-#     ui_table.style = 'table_ui_title' #ui_table._TableStyle('table_ui')
-#
-#     doc.save(f"{os.sep}test_ui_table.docx")
+import docx
+with open(r'/home/risovaniy/auto_UI_UA/resources/UA_template_utf_8.docx',) as f_1:
+    with open(r'/home/risovaniy/auto_UI_UA/resources/UI_template.docx',
+              'a',
+              ) as f:
 
-
-if __name__ == '__main__':
-
-
-
-
-
-
-
-
-
+        f.write(f_1.readlines())
