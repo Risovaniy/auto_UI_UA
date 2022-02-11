6a4e9b5ed5a4d5532538184c920fe2d5e6e58753
SergeyRisovaniy <risovaniysa@gmail.com>
2022-02-11 14:00:31 +0300

    Found a working way to copy a document

--- trash/checking_testing_hipo.py
+++ trash/checking_testing_hipo.py
@@ -1,7 +1,24 @@
-import docx
-with open(r'/home/risovaniy/auto_UI_UA/resources/UA_template_utf_8.docx',) as f_1:
-    with open(r'/home/risovaniy/auto_UI_UA/resources/UI_template.docx',
-              'a',
-              ) as f:
+from docx import Document
 
-        f.write(f_1.readlines())
+files = [r'/home/risovaniy/auto_UI_UA/resources/UI_template_utf_8.docx',
+         r'/home/risovaniy/auto_UI_UA/resources/UA_template_utf_8.docx']
+
+
+def combine_word_documents(files):
+    merged_document = Document()
+
+    for index, file in enumerate(files):
+        sub_doc = Document(file)
+
+        if index < len(files) - 1:
+            sub_doc.add_page_break()
+
+        for element in sub_doc.element.body:
+            merged_document.element.body.append(element)
+
+    merged_document.save('d3.docx')
+
+
+combine_word_documents(files)
+
+
